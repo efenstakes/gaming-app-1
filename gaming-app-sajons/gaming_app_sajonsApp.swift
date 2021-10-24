fc01fd71ed5ef3b2b18fe87557079c987eca7672
kipchumba26 <cynthia.kipchumba@nativecoders.se>
2021-10-24 22:32:41 +0300

    Initial Commit

---
+++ gaming-app-sajons/gaming_app_sajonsApp.swift
@@ -0,0 +1,17 @@
+//
+//  gaming_app_sajonsApp.swift
+//  gaming-app-sajons
+//
+//  Created by MAC on 10/24/21.
+//
+
+import SwiftUI
+
+@main
+struct gaming_app_sajonsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
